95bcd6b18e7c18f996a41d122f3905f074d52c5f
mahmoud <mahmoud63@users.noreply.github.com>
2022-03-12 23:48:06 +0200

    d

--- test/unit/helpers/utils.spec.js
+++ test/unit/helpers/utils.spec.js
@@ -510,8 +510,8 @@ describe('Helpers: Utils', () => {
   });
 
   it('Should check hours count between two dates greater than 1', () => {
-    const digits = Utils.differenceBetweenTwoDatesGreaterThanHour('2022-03-12 21:22:17.532+00');
-    expect(typeof digits).to.be.eq('boolean');
-    expect(digits.length).to.be.eq(false);
+    const flag = Utils.differenceBetweenTwoDatesGreaterThanHour('2022-03-12 21:22:17.532+00');
+    expect(typeof flag).to.be.eq('boolean');
+    expect(flag).to.be.eq(false);
   });
 });
